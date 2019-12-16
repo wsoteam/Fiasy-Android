d4e012dab124b5a76fd0a16b53b0677084f8c4ca
Wadim <waadimka@mail.ru>
2019-12-16 19:23:12 +0300

    custom holder
    https://wsoteam.atlassian.net/browse/TN-16828

--- app/src/main/java/com/wsoteam/diet/presentation/search/sections/custom/fragments/controller/CustomPortionsVH.java
+++ app/src/main/java/com/wsoteam/diet/presentation/search/sections/custom/fragments/controller/CustomPortionsVH.java
@@ -2,17 +2,15 @@ package com.wsoteam.diet.presentation.search.sections.custom.fragments.controlle
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.wsoteam.diet.R;
 
 import androidx.recyclerview.widget.RecyclerView;
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class StandartPortionsVH extends RecyclerView.ViewHolder {
-    public StandartPortionsVH(LayoutInflater layoutInflater, ViewGroup viewGroup) {
-        super(layoutInflater.inflate(R.layout.item_portions_standart, viewGroup, false));
+public class CustomPortionsVH extends RecyclerView.ViewHolder {
+    public CustomPortionsVH(LayoutInflater layoutInflater, ViewGroup viewGroup) {
+        super(layoutInflater.inflate(R.layout.item_portions_custom, viewGroup, false));
         ButterKnife.bind(this, itemView);
     }
 }
