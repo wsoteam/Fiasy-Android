65d2d944f309a9b6919ee46c8eba5d3429cc5435
Wadim <waadimka@mail.ru>
2019-12-16 19:02:04 +0300

    add holder adder
    https://wsoteam.atlassian.net/browse/TN-16826

--- app/src/main/java/com/wsoteam/diet/presentation/search/sections/custom/fragments/controller/AddPortionsVH.java
+++ app/src/main/java/com/wsoteam/diet/presentation/search/sections/custom/fragments/controller/AddPortionsVH.java
@@ -1,4 +1,16 @@
 package com.wsoteam.diet.presentation.search.sections.custom.fragments.controller;
 
-public class AddPortionsVH {
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.wsoteam.diet.R;
+
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.ButterKnife;
+
+public class AddPortionsVH extends RecyclerView.ViewHolder {
+    public AddPortionsVH(LayoutInflater layoutInflater, ViewGroup viewGroup) {
+        super(layoutInflater.inflate(R.layout.item_portions_add, viewGroup, false));
+        ButterKnife.bind(this, itemView);
+    }
 }
