a5214a46383a7dd8d268c817ceb0f39e7884c91c
Wadim <waadimka@mail.ru>
2019-12-19 17:49:10 +0300

    formula
    https://wsoteam.atlassian.net/browse/TN-17062

--- app/src/main/java/com/wsoteam/diet/presentation/search/sections/custom/fragments/FragmentOutlay.java
+++ app/src/main/java/com/wsoteam/diet/presentation/search/sections/custom/fragments/FragmentOutlay.java
@@ -1,26 +1,27 @@
 package com.wsoteam.diet.presentation.search.sections.custom.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.wsoteam.diet.BranchOfAnalyzer.CustomFood.CustomFood;
-import com.wsoteam.diet.R;
-import com.wsoteam.diet.common.networking.food.POJO.Result;
-import com.wsoteam.diet.presentation.search.sections.custom.ActivityCreateFood;
-import com.wsoteam.diet.presentation.search.sections.custom.SayForward;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import com.wsoteam.diet.BranchOfAnalyzer.CustomFood.CustomFood;
+import com.wsoteam.diet.R;
+import com.wsoteam.diet.common.networking.food.POJO.Result;
+import com.wsoteam.diet.presentation.search.sections.custom.ActivityCreateFood;
+import com.wsoteam.diet.presentation.search.sections.custom.SayForward;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentOutlay extends Fragment implements SayForward {
 
@@ -28,9 +29,11 @@ public class FragmentOutlay extends Fragment implements SayForward {
   @BindView(R.id.edtFats) EditText edtFats;
   @BindView(R.id.edtCarbo) EditText edtCarbo;
   @BindView(R.id.edtProt) EditText edtProt;
+  @BindView(R.id.tvAlert) TextView tvAlert;
   private Result result;
   private final int FAT_SIZE = 9;
   private final int OTHER_SIZE = 4;
+  private List<EditText> fields = new ArrayList<>();
 
   Unbinder unbinder;
   private final double EMPTY_PARAM = -1.0;
@@ -85,9 +88,95 @@ public class FragmentOutlay extends Fragment implements SayForward {
       @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_outlay_new, container, false);
     unbinder = ButterKnife.bind(this, view);
+    fields.add(edtKcal);
+    fields.add(edtFats);
+    fields.add(edtCarbo);
+    fields.add(edtProt);
     result = ((ActivityCreateFood) getActivity()).customFood;
-    isRightFormula();
+    setListeners();
     return view;
+  }
+
+  private void setListeners() {
+    edtCarbo.addTextChangedListener(new TextWatcher() {
+      @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+      }
+
+      @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (checkReady()) {
+          showBottomAlert(isRightFormula());
+        }
+      }
+
+      @Override public void afterTextChanged(Editable s) {
+
+      }
+    });
+    edtFats.addTextChangedListener(new TextWatcher() {
+      @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+      }
+
+      @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (checkReady()) {
+          showBottomAlert(isRightFormula());
+        }
+      }
+
+      @Override public void afterTextChanged(Editable s) {
+
+      }
+    });
+    edtKcal.addTextChangedListener(new TextWatcher() {
+      @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+      }
+
+      @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (checkReady()) {
+          showBottomAlert(isRightFormula());
+        }
+      }
+
+      @Override public void afterTextChanged(Editable s) {
+
+      }
+    });
+    edtProt.addTextChangedListener(new TextWatcher() {
+      @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+      }
+
+      @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (checkReady()) {
+          showBottomAlert(isRightFormula());
+        }
+      }
+
+      @Override public void afterTextChanged(Editable s) {
+
+      }
+    });
+  }
+
+  private void showBottomAlert(boolean rightFormula) {
+    if (rightFormula) {
+      tvAlert.setVisibility(View.INVISIBLE);
+    } else {
+      tvAlert.setVisibility(View.VISIBLE);
+    }
+  }
+
+  private boolean checkReady() {
+    boolean isReady = true;
+    for (EditText editText : fields) {
+      editText.getText().toString().replaceAll("\\s+", " ").trim();
+      if (editText.getText().toString().equals("")) {
+        isReady = false;
+      }
+    }
+    return isReady;
   }
 
   private boolean isRightFormula() {
